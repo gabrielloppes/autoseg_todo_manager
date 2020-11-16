f26265ef028bda132346251c7f04cd88d0015398
Gabriel Lopes <gabriellopees@hotmail.com>
2020-11-16 02:46:00 -0300

    Adding actions to task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,24 @@
 class TasksController < ApplicationController
+  # Permite alterar o status para feita
+  def set_id_done
+    @task = Task.find(params[:task_id])
+    @task.update(status: :closed)
+    flash[:notice] = "'#{task.description}' marked as done"
+    redirect_to todo_list_path(@task.todo_list)
+  end
+
+  # Permite alterar o status para não feita
+  def set_id_undone
+    @task = Task.find(params[:task_id])
+    @task.update(status: :open)
+    flash[:notice] = "'#{task.description}' marked as undone"
+    redirect_to todo_list_path(@task.todo_list)
+  end
+
+  # Permite deletar uma task
+  def destroy 
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to edit_todo_list_path(params[:todo_list_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
     put :create_task
     resources :taks, only: [:set_it_done, :set_it_undone, :destroy] do
       #  PUT atualiza um recurso existente
-      put :make_it_done
-      put :make_it_undone
+      put :set_it_done
+      put :set_it_undone
     end
     resources :favorites, only: [:index, :create, :destroy]
     get '/locate_list', to: 'todo_list#locate_list'
