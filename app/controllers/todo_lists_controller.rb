6f0f24f211781546934c6d8db909837be89a8588
Gabriel Lopes <gabriellopees@hotmail.com>
2020-11-16 02:38:19 -0300

    Adding actions to todo_lists controller

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -5,7 +5,12 @@ class TodoListsController < ApplicationController
   end
 
   def show
-
+    # Verifica se o usuário que está tentando acessar uma lista possui os requisitos.
+    if !check_access?
+      flash[:alert] = "You don't have access to this list"
+      redirect_to root_path
+    end
+    return @todo_list, @favorites_ids
   end
 
   # No momento da criação o usuário pode criar uma lista e ao mesmo tempo criar tarefas para essa lista
@@ -36,6 +41,7 @@ class TodoListsController < ApplicationController
     if @todo_list.update(todo_list_params)
       redirect_to todo_lists_path
       flash[:notice] = "'#{@todo_list.name}' was update"
+      redirect_to todo_list_path(@todo_list)
     else
       render :edit
     end
@@ -43,46 +49,61 @@ class TodoListsController < ApplicationController
 
   # Torna uma lista privada
   def make_it_private
-
+    @todo_list.update(status: :personal)
+    flash[:notice] = "'#{@todo_list.name}' is now private"
+    redirect_to todo_list_path(@todo_list)
   end
 
   # Torna uma lista publica
   def make_it_public
-
+    @todo_list.update(status: :shareable)
+    flash[:notice] = "'#{@todo_list.name}' is now public"
+    redirect_to todo_list_path(@todo_list)
   end
 
   def destroy
-
+    if @todo_list.destroy
+      redirect_to todo_list_url
+      flash[:alert] = "'#{@todo_list.name}' was removed"
+    end
   end
 
   def create_task
-
+    @todo_list.update(todo_list_params)
+    redirect_to @todo_list
   end
 
+  # Mostra as listas que são publicas e por ordem de criação
   def locate_lists
-
+    @todo_lists = TodoList.where(status: :shareable).where.not(user_id: current_user.id).order(created_at: :desc)
   end
 
   private
 
   # Verifica se o usuário possui direito de acesso à uma lista
   def check_access
-
+    @todo_list = TodoList.find(params[:id])
+    # Verifica se o usuário é o dono da lista(terá acesso pois é o criador) ou verifica se a lista é pública, desta maneira qualquer usuário terá acesso
+    (@todo_list.user == current_user || @todo_list.shareable?) ? true : false
   end
 
   def locate_favorites
-
+    # Passa um bloco como argumento e usa dentro dentro do método
+    @favorites = current_user.favorites.map(&:todo_list)
+    @favorites_ids = []
+    @favorites.each { |item| @favorites_ids << item.id }
+    return @favorites_ids
   end
 
   def set_todo_list
-
+    @todo_list = TodoList.find(params[:id])
   end
 
   def todo_list_params
-
+    params.require(:todo_list).permit(:name, task_items_attributes: [:id, :description])
   end
 
   def set_todo_list_id
-
+    @todo_list = TodoList.find(params[:todo_list_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :todo_lists do
+    #  PUT atualiza um recurso existente
+    put :make_it_public
+    put :make_it_private
+    put :create_task
+    resources :taks, only: [:set_it_done, :set_it_undone, :destroy] do
+      #  PUT atualiza um recurso existente
+      put :make_it_done
+      put :make_it_undone
+    end
+    resources :favorites, only: [:index, :create, :destroy]
+    get '/locate_list', to: 'todo_list#locate_list'
+  end
   root to: "todo_list#index"
 end
