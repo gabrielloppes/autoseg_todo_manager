19eb57e1934defb655558b40a7eaf87fa85b330e
Gabriel Lopes <gabriellopees@hotmail.com>
2020-11-24 12:07:33 -0300

    Added favorites controller to the project

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,23 @@
 class FavoritesController < ApplicationController
-  def update
-    @favorite = Favorite.where(todo_list: TodoList.find(params[:todo_list]), user: current_user)
-    if favorite == []
-      # Cria o favorito
-      Favorite.create(todo_list: TodoList.find(params[:todo_list]), user: current_user)
-      @favorite_exists = true
-    else
-      # Deleta o favorito(s)
-      favorite.destroy_all
-      @favorite_exists = false
-      respond_to do |format|
-        format.html {}
-        format.js {}
-      end
-    end
+  # Mostra todas as listas marcadas como favoritas
+  def index 
+    @favorires = current_user.favorites.collect(&:todo_list)
+  end
+
+  # Permite criar uma lista como favorite
+  def create
+    @favorite = Favorite.create(todo_list_id: params[:todo_list_id])
+    @favorite.user = current_user
+    @favorite.save
+    flash[:notice] = "List marked as favorite"
+    redirect_to todo_list_path(@favorite.todo_list)
+  end
+
+  # Quando o usuário desmarcar uma lista como 'favorita' ela voltará para a pagina principal com o 'redirect_back']
+  def destroy
+    @favorite = Favorite.find_by(todo_list_id: params[:todo_list_id], user_id: current_user.id)
+    @favorite.destroy
+    flash[:alert] = "List was removed from favorites"
+    redirect_back(fallback_location: todo_list_path(params[:todo_list_id]))
   end
 end
